dd11b2256816c013578a6aa2544c8fddfc2d34fb
Otstoy <otstoy.sxe@gmail.com>
2025-08-07 14:56:24 +0300

    Implemented admin models for django admin.

---
+++ taxi/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from taxi_service import settings
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    country = models.CharField(max_length=63)
+
+    def __str__(self):
+        return f"{self.name} {self.country}"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name="cars"
+    )
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
+
+    def __str__(self):
+        return f"{self.manufacturer.name} {self.model} {self.drivers.name}"
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.license_number}"
